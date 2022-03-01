29fa2333abb23f3f7fe6401364edec1710c8f7b5
SShekotihin <sshekotihindev@gmail.com>
2022-03-01 10:57:13 +0300

    tmpfile

--- lib/unleash/toggle_fetcher.rb
+++ lib/unleash/toggle_fetcher.rb
@@ -67,13 +67,14 @@ module Unleash
       Unleash.logger.debug "Will save toggles to disk now"
       begin
         backup_file = Unleash.configuration.backup_file
-        backup_file_tmp = "#{backup_file}.tmp"
 
         self.toggle_lock.synchronize do
-          file = File.open(backup_file_tmp, "w")
+          file = Tempfile.new
+          path = file.path
+
           file.write(self.toggle_cache.to_json)
           file.close
-          File.rename(backup_file_tmp, backup_file)
+          File.rename(path, backup_file)
         end
       rescue StandardError => e
         # This is not really the end of the world. Swallowing the exception.
